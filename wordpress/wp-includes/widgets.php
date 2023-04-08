9b8784ec614b45e16281676dbb282b8b3113cd9d
SonvanJune <sonthu3333@gmail.com>
2023-04-08 09:05:18 +0700

    change footer

--- wordpress/wp-includes/widgets.php
+++ wordpress/wp-includes/widgets.php
@@ -1844,7 +1844,7 @@ function wp_widgets_init() {
 
 	register_widget( 'WP_Widget_Custom_HTML' );
 
-	register_widget( 'WP_Widget_Block' );
+    register_widget( 'WP_Widget_Block' );
 
 	/**
 	 * Fires after all default WordPress widgets have been registered.
